0f943e1047ccd3378b262fa073b478599bd5f557
JananiR23 <61050498+JananiR23@users.noreply.github.com>
2020-06-09 18:02:23 +0530

    Retrieve attribute value

--- src/main/java/com/rbs/testharness/repository/PricingBusinessAttributeRepository.java
+++ src/main/java/com/rbs/testharness/repository/PricingBusinessAttributeRepository.java
@@ -1,11 +1,16 @@
 package com.rbs.testharness.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.rbs.testharness.entity.PricingBusinessAttributeEntity;
+
 
 @Repository
 public interface PricingBusinessAttributeRepository extends JpaRepository<PricingBusinessAttributeEntity, Integer>{
+	List<PricingBusinessAttributeEntity> findByAttributeIdIn(List<Integer> attributeIds);
 
+	
 }
+
