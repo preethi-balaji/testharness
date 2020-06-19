703ea7281cafe4056681d23026799a78b1d10fec
JananiR23 <61050498+JananiR23@users.noreply.github.com>
2020-06-19 09:46:33 +0530

    Added Overdraft and Agri columns

--- src/main/java/com/rbs/testharness/entity/PricingLookUpEntity.java
+++ src/main/java/com/rbs/testharness/entity/PricingLookUpEntity.java
@@ -2,6 +2,8 @@ package com.rbs.testharness.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class PricingLookUpEntity extends BaseEntity{
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "THS_PRC_LOOKUP_ID")
 	private Integer lookUpId;
 	@Column(name = "RISK_BAND")
@@ -22,6 +25,49 @@ public class PricingLookUpEntity extends BaseEntity{
 	private Double airRate;
 	@Column(name = "APR_RATE")
 	private Double aprRate;
+	
+	@Column(name = "MARGIN_FEE")
+	private Double marginFee;
+	@Column(name = "ARRANGEMENT_FEE")
+	private Double arrangementFee;
+	@Column(name = "START_MARGIN_FEE")
+	private Double startMarginFee;
+	@Column(name = "TERM_MARGIN_PREMIUM")
+	private Double termMarginPremium;
+	@Column(name = "THS_PRC_ATTRIBUTE_ID")
+	private Integer attributeId;
+	
+
+	public Integer getAttributeId() {
+		return attributeId;
+	}
+	public void setAttributeId(Integer attributeId) {
+		this.attributeId = attributeId;
+	}
+	public Double getMarginFee() {
+		return marginFee;
+	}
+	public void setMarginFee(Double marginFee) {
+		this.marginFee = marginFee;
+	}
+	public Double getArrangementFee() {
+		return arrangementFee;
+	}
+	public void setArrangementFee(Double arrangementFee) {
+		this.arrangementFee = arrangementFee;
+	}
+	public Double getStartMarginFee() {
+		return startMarginFee;
+	}
+	public void setStartMarginFee(Double startMarginFee) {
+		this.startMarginFee = startMarginFee;
+	}
+	public Double getTermMarginPremium() {
+		return termMarginPremium;
+	}
+	public void setTermMarginPremium(Double termMarginPremium) {
+		this.termMarginPremium = termMarginPremium;
+	}
 	
 	public Integer getLookUpId() {
 		return lookUpId;
